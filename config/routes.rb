a4d1e544652eb91cade035485335e218af793156
bdsylva <bethany.dsylva@students.ebs.edu>
2017-05-21 19:37:12 +0000

    2.8 contact and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'about' => "static_pages#about"
+
+  get 'contact' => "static_pages#contact"
+
+  get 'index' => "static_pages#index"
+  root 'static_pages#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
